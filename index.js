413af700296f3f2d26a56862fa13fcaf50ef79e3
Ravi Meena <amiableravi2893@gmail.com>
2023-03-26 20:47:48 +0530

    my commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 2000;
+app.get('/', (req, res) => {
+    const hex = "0123456789ABCDEF";
+    const color = `#${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}`;
+    res.send(color);
+});
+app.listen(port, () => {
+    console.log("Your personal random color generator is running !!");
+});
