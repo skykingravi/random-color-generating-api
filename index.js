71dd4107843c8c9854b9de54d6d31878525e4fbd
Ravi Meena <amiableravi2893@gmail.com>
2023-03-26 20:53:51 +0530

    my commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 2000;
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     const hex = "0123456789ABCDEF";
     const color = `#${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}${hex[Math.floor(Math.random()*15)]}`;
     res.send(color);
